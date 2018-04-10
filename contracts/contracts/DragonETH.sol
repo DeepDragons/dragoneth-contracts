3899146a0ac1b2047c5920fd0e0bb47cbfece4ee
Igor Khan <khanigot@gmail.com>
2018-04-10 11:10:41 +0500

    Redisigne main contract, change to Role Based Access Control and create GameControl contract

--- contracts/contracts/DragonETH.sol
+++ contracts/contracts/DragonETH.sol
@@ -1,21 +1,18 @@
 pragma solidity ^0.4.21;
 
 import "./ERC721/ERC721Token.sol";
-import "./ContractInterface.sol";
-import "./security/Whitelist.sol";
+import "./DragonETHgameControl.sol";
 
-contract DragonETH is ERC721Token("DragonETH", "DETH"), Whitelist {
+contract DragonETH is ERC721Token("DragonETH", "DETH"), DragonETHgameControl {
     uint256 public totalDragons;
     struct Dragon {
         uint256 gen1;
         uint256 gen2;
     }
     Dragon[] dragons;
-    // Re designe?
-    GenRNG private genRNGContractAddress;
-    FixMarketPlace public fmpContractAddress;
-
-    function DragonETH(address _genRNGContractAddress, address _fmpContractAddress) public {
+    
+   
+    function DragonETH() public {
  
         _mint(msg.sender, 0);
         Dragon memory _dragon = Dragon({
@@ -23,18 +20,8 @@ contract DragonETH is ERC721Token("DragonETH", "DETH"), Whitelist {
             gen2: 0
         });
         dragons.push(_dragon);
-        genRNGContractAddress = GenRNG(_genRNGContractAddress);
-        fmpContractAddress = FixMarketPlace(_fmpContractAddress);
     }
-    // Re designe?
-    function changeRNGcontractAddress(address _genRNGContractAddress) external onlyOwner {
-        genRNGContractAddress = GenRNG(_genRNGContractAddress);
-    }
-    // Re designe?
-    function changeFMPcontractAddress(address _fmpContractAddress) external onlyOwner {
-        fmpContractAddress = FixMarketPlace(_fmpContractAddress);
-    }
-    // Re designe?
+   
     function addToFixMarketPlace(uint256 _dragonID, uint256 _dragonPrice, uint256 _endBlockNumber) external  {
         require(msg.sender == ownerOf(_dragonID));
         if (fmpContractAddress.addToFixMarketPlace(msg.sender, _dragonID, _dragonPrice, _endBlockNumber)) {
@@ -47,8 +34,9 @@ contract DragonETH is ERC721Token("DragonETH", "DETH"), Whitelist {
     _res1 = bytes32(dragons[_dragonID].gen1);
     _res2 = bytes32(dragons[_dragonID].gen2);
     }
-    function createDragon(address _to) external onlyWhitelisted() {
+    function createDragon(address _to) external onlyRole("CreateAgent") {
         totalDragons++;
+        // TODO add chek to safeTransfer
         _mint(_to, totalDragons);
         uint256[2] memory twoGen = genRNGContractAddress.getNewGens(_to, totalDragons);
         Dragon memory _dragon = Dragon({
