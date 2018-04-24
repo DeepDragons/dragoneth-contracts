f1512368ca1a8daaeaabd2b9dca252d85a3134ac
IgorKhan <khanigot@gmail.com>
2018-04-25 04:27:47 +0500

    beginin mutagen2Face.sol

--- contracts/contracts/Mutagen2Face.sol
+++ contracts/contracts/Mutagen2Face.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.4.23;
 
+contract DragonsETH {
+     function killDragonDeathContract(address _lastOwner, uint256 _dragonID, uint256 _deathReason) external;
+     function transferFrom(address _from, address _to, uint256 _tokenId) public;
+}
+contract CheckMutagenResistance {
+    function checkResistance(uint256 _dragonID) external view returns (bool);
+}
+
 
 contract Mutagen2Face {
     function addDragon(address _dragonOwner, uint256 _dragonID, uint256 mutagenCount) external;
