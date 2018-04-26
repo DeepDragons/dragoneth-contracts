ca1f7537bcb991418327a842f0c798ddc5293fb6
Igor Khan <khanigot@gmail.com>
2018-04-27 00:16:24 +0500

    change game name

--- contracts/contracts/DragonsETH.sol
+++ contracts/contracts/DragonsETH.sol
@@ -4,9 +4,9 @@ import "./ERC721/ERC721Token.sol";
 import "./DragonsETH_GC.sol";
 import "./security/ReentrancyGuard.sol";
 
-// FOR TEST remove in war deploy
-// contract DragonsETH is ERC721Token("DragonsETH.com game", "DragonsETH"), DragonsETH_GC, ReentrancyGuard {
-contract DragonsETH is ERC721Token("Test game", "Test"), DragonsETH_GC, ReentrancyGuard {
+
+contract DragonsETH is ERC721Token("DragonsETH.com Dragon", "DragonsETH"), DragonsETH_GC, ReentrancyGuard {
+
     uint256 public totalDragons;
     uint256 public liveDragons;
     struct Dragon {
