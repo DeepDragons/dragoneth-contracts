9c6bab8591d311427ad151d7352b3ff4eb3fcdee
Igor Khan <khanigot@gmail.com>
2018-06-28 12:47:33 +0500

    full realisation

--- contracts/contracts/private/ProxyWallet.sol
+++ contracts/contracts/private/ProxyWallet.sol
@@ -1,2 +1,17 @@
 pragma solidity ^0.4.24;
 
+import "../security/rbac/RBACWithAdmin.sol";
+
+contract ProxyWallet is RBACWithAdmin {
+    address mainWallet;
+    
+    constructor(address _mainWallet) public {
+        mainWallet = _mainWallet;
+    }
+    function() public payable {
+        mainWallet.transfer(msg.value);
+    }
+    function changeMainWallet(address _mainWallet) external onlyAdmin {
+        mainWallet = _mainWallet;
+    }
+}
