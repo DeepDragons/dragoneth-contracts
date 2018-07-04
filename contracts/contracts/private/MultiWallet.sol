96c6b410c20a838f7a08a1645b8594d81fa63969
Igor Khan <khanigot@gmail.com>
2018-07-04 12:30:19 +0500

    bug fix and change address

--- contracts/contracts/private/MultiWallet.sol
+++ contracts/contracts/private/MultiWallet.sol
@@ -6,11 +6,11 @@ import "../security/ReentrancyGuard.sol";
 contract MultiWallet is ReentrancyGuard {
     address wallet1_7 = 0x98F04b28946C73D049c04A63Ac1F3352689F23bf; // Renat
     address wallet2_7 = 0x36d9399A33C4851526D541E0e4B577E5dAdc6be0; // Igor
-    address wallet3_7 = 0x971bCD2823ab07b39eA765Bd5863A4e63e0c7205; // Alexander
+    address wallet3_7 = 0x4044f3164AE9465c249963ffF008420171a3E522; // Alexander
     address wallet4_7 = 0x181d493A0499E81726FB7dfFa698d2923e780669; // Ivan
-    address wallet5_2  = 0x27DC375Ce528832B0b1955A288c411CaE4Ac7c12; // Kate tmp Igor
-    address wallet6_10 = 0xbF588e41A13314b84C22b3e2215f56DefE09C638; // disgner department 10% tmp Alexander
-    address wallet7_8  = 0xFF996042E1f9e11c9658e5bEdce2363F853490F1; //  NN change wallet
+    address wallet5_2 = 0x27DC375Ce528832B0b1955A288c411CaE4Ac7c12; // Kate tmp Igor
+    address wallet6_10 = 0xfa01d5584B39A46f384168e32A86Ae2d48F7D1Aa; // disgner department 10% tmp Alexander
+    address wallet7_8  = 0x888430ed4d28603286d59eDD12B3C1afCCe35657; //  NN change wallet
     address wallet8_309 = 0xFF996042E1f9e11c9658e5bEdce2363F853490F1; //  prodaction work Ivan
     address wallet9_005 = 0x246C5881E3F109B2aF170F5C773EF969d3da581B; // Rinat
     address wallet10_206 = 0xb2406fe92dA73f6ed175862007b5cb7d781d881D; //  prodaction invester return tmp Igor
@@ -35,7 +35,7 @@ contract MultiWallet is ReentrancyGuard {
         wallet7_8.transfer(w8);
         wallet8_309.transfer(w309);
         wallet9_005.transfer(w05);        
-        wallet10_206.transfer(totalBalance - w7 * 4 - w2 - w10 - w8 - w05);
+        wallet10_206.transfer(totalBalance - w7 * 4 - w2 - w10 - w8 - w309 - w05);
    }
    function changeWallet1(address _newWallet) external {
        require(msg.sender == wallet1_7);
