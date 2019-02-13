e91ce882c33b53ac5fcdb0393fdd8b1b8ad18c6b
Igor Khan <khanigot@gmail.com>
2019-02-13 20:42:06 +0500

    change code to Solidity ver. 0.5.4

--- contracts/contracts/private/DragonsFight.sol
+++ contracts/contracts/private/DragonsFight.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.5.4;
 
 import "../security/rbac/RBACWithAdmin.sol";
 
@@ -6,7 +6,7 @@ contract DragonsFight is RBACWithAdmin {
     function getWinner(uint256 _dragonOneID, uint256 _dragonTwoID) external view returns (uint256 _winerID){
     // TODO Develop It!!!!
         require(_dragonOneID != _dragonTwoID);
-        if (uint8(blockhash(block.number-2)) > 127 ) {
+        if (uint8(uint(blockhash(block.number-2))) > 127 ) {
             return _dragonOneID;    
         } else {
             return _dragonTwoID;
