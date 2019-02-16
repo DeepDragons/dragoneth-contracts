c5541f56e042edc2d92b409af8d56d5e1473f3d0
Igor Khan <khanigot@gmail.com>
2019-02-16 19:06:59 +0500

    remove unused functions and vars

--- contracts/contracts/fight/DragonsFightGC.sol
+++ contracts/contracts/fight/DragonsFightGC.sol
@@ -11,15 +11,12 @@ contract DragonsETH {
         uint240 gen2;
         uint256 nextBlock2Action;
     }
-//mybe function????
     Dragon[] public dragons;
     
     function ownerOf(uint256 _tokenId) public view returns (address);
     function checkDragonStatus(uint256 _dragonID, uint8 _stage) public view;
     function setCurrentAction(uint256 _dragonID, uint8 _currentAction) external;
     function setTime2Rest(uint256 _dragonID, uint256 _addNextBlock2Action) external;
-    function transferFrom(address _from, address _to, uint256 _tokenId) public;
-    function killDragonDeathContract(address _lastOwner, uint256 _dragonID, uint256 _deathReason) external;
     function isApprovedOrOwner(address _spender, uint256 _tokenId) public view returns (bool);
 
     
@@ -32,7 +29,6 @@ contract DragonsFight {
 contract DragonsStats {
     function incFightWin(uint256 _dragonID) external;
     function incFightLose(uint256 _dragonID) external;
-    function incFightToDeathWin(uint256 _dragonID) external;
     function setLastAction(uint256 _dragonID, uint256 _lastActionDragonID, uint8 _lastActionID) external;
 }
 
@@ -41,17 +37,13 @@ contract Mutagen {
 }
 
 contract DragonsFightGC is Pausable {
-//    using SafeMath for uint256;
     Mutagen public mutagenContract;
     DragonsETH public mainContract;
     DragonsFight public dragonsFightContract;
     DragonsStats public dragonsStatsContract;
     address payable wallet;
-    uint256 public minFightWaitBloc = 80; // ~20 min
-    uint256 public maxFightWaitBloc = 172800; // ~30 days??????
     uint256 public mutagenToWin = 10;
     uint256 public mutagenToLose =1;
-    uint256 public mutagenToDeathWin = 100;
     uint256 public addTime2Rest = 240; // ~ 60 min
     
     event FightFP(uint256 _winnerId, uint256 _loserId, address _ownerWinner, address _onwerLoser);
@@ -74,25 +66,12 @@ contract DragonsFightGC is Pausable {
     function changeWallet(address payable _wallet) external onlyAdmin {
         wallet = _wallet;
     }
-
-    function changeMinFightWaitBloc(uint256 _minFightWaitBloc) external onlyAdmin {
-        minFightWaitBloc = _minFightWaitBloc;
-    }
-
-    function changeMaxFightWaitBloc(uint256 _maxFightWaitBloc) external onlyAdmin {
-        maxFightWaitBloc = _maxFightWaitBloc;
-    }
-    
     function changeMutagenToWin(uint256 _mutagenToWin) external onlyAdmin {
         mutagenToWin = _mutagenToWin;
     }
     
     function changeMutagenToLose(uint256 _mutagenToLose) external onlyAdmin {
         mutagenToLose = _mutagenToLose;
-    }
-    
-    function changeMutagenToDeathWin(uint256 _mutagenToDeathWin) external onlyAdmin {
-        mutagenToDeathWin = _mutagenToDeathWin;
     }
     function changeAddTime2Rest(uint256 _addTime2Rest) external onlyAdmin {
         addTime2Rest = _addTime2Rest;
