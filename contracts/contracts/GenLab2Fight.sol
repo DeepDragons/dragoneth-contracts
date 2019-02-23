c1a9a8f1e3a61aacddb6a5c1d23f13da5433b9d9
Igor Khan <khanigot@gmail.com>
2019-02-23 18:38:28 +0500

    cp from Mutagen2Fight.sol

---
+++ contracts/contracts/GenLab2Fight.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.5.4;
+
+//????
+import "./security/rbac/RBACWithAdmin.sol";
+
+contract DragonsETH {
+    struct Dragon {
+        uint256 gen1;
+        uint8 stage; // 0 - Dead, 1 - Egg, 2 - Young Dragon 
+        uint8 currentAction; // 0 - free, 1 - fight place, 0xFF - Necropolis,  2 - random fight,
+                             // 3 - breed market, 4 - breed auction, 5 - random breed, 6 - market place ...
+        uint240 gen2;
+        uint256 nextBlock2Action;
+    }
+
+    Dragon[] public dragons;
+
+    function ownerOf(uint256 _tokenId) public view returns (address _owner);
+    function setTime2Rest(uint256 _dragonID, uint256 _addNextBlock2Action) external;
+    function isApprovedOrOwner(address _spender, uint256 _tokenId) public view returns (bool);
+    function changeDragonGen(uint256 _dragonID, uint256 _gen, uint8 _which) external;
+}
+contract Mutagen {
+    function burn(address _from, uint256 _value) public;
+     function balanceOf(address _owner) public view returns (uint256 balance);
+}
+
+contract RNG {
+    function get32b(address _from, uint256 _dragonID) external returns (bytes32 b32);
+}
+
+contract Mutagen2Fight is RBACWithAdmin {
+    Mutagen public mutagenContract;
+    DragonsETH public mainContract;
+    address private addressRNG;
+    uint256 public addTime2Rest = 240; // ~ 60 min
+     
+    event FightGensChanged(uint256 _dragonOneID, uint240 _oldGens, uint240 _newGens);
+
+    constructor(address _addressMainContract, address _addressMutagen) public {
+        mainContract = DragonsETH(_addressMainContract);
+        mutagenContract = Mutagen(_addressMutagen);
+    }
+    function mutateFightGens(uint256 _dragonID, uint256 _mutagenCount) external {
+        require(mutagenContract.balanceOf(msg.sender) >= _mutagenCount);
+        require(mainContract.ownerOf(_dragonID) == msg.sender);
+        
+    }
+    function changeAddTime2Rest(uint256 _addTime2Rest) external onlyAdmin {
+        addTime2Rest = _addTime2Rest;
+    }
+    function changeAddressRNG(address _addressRNG) external onlyAdmin {
+        addressRNG = _addressRNG;
+    }
+}
+
