536291984b4602e249d706c727e86cbff6a953b2
Igor Khan <khanigot@gmail.com>
2019-02-28 15:28:42 +0500

    refactoring, fix bug burn mutagen, add text to require

--- contracts/contracts/GenLab2Fight.sol
+++ contracts/contracts/GenLab2Fight.sol
@@ -16,12 +16,10 @@ contract DragonsETH {
 
     function ownerOf(uint256 _tokenId) public view returns (address _owner);
     function setTime2Rest(uint256 _dragonID, uint256 _addNextBlock2Action) external;
-    function isApprovedOrOwner(address _spender, uint256 _tokenId) public view returns (bool);
     function changeDragonGen(uint256 _dragonID, uint256 _gen, uint8 _which) external;
 }
 contract Mutagen {
     function burn(address _from, uint256 _value) public;
-    function balanceOf(address _owner) public view returns (uint256 balance);
 }
 
 contract DragonsStats {
@@ -39,7 +37,7 @@ contract GenLab2Fight is RBACWithAdmin {
     DragonsStats public dragonsStatsContract;
     address private addressRNG;
     address payable wallet;
-    uint256 public addTime2Rest = 240; // ~ 60 min
+    uint256 public addTime2Rest = 0;
     uint256 public mutagenCount = 100;
     uint256 public priceMutagenWork = 0.1 ether;
     uint256 public priceMax = 1 ether;
@@ -53,53 +51,52 @@ contract GenLab2Fight is RBACWithAdmin {
         wallet = _wallet;
     }
     function setMaxGen(uint256 _dragonID, uint256 _genNum) external payable {
-        //add check to actions and refactor it
-        require(msg.value >= priceMax);
-        require(mainContract.ownerOf(_dragonID) == msg.sender);
-        require(_genNum <= 29);
-        uint256 returnValue = msg.value - priceMax;
-        wallet.transfer(priceMax);
-        if (returnValue > 0) {
-            msg.sender.transfer(returnValue);
-        }
+        _firstCheck(_dragonID, _genNum, priceMax);
+        uint8 currentAction;
         uint240 gensDragon;
         uint256 nextBlock2Action;
-        (,,,gensDragon,nextBlock2Action) = mainContract.dragons(_dragonID);
-        require(nextBlock2Action <= block.number);
+        (,,currentAction,gensDragon,nextBlock2Action) = mainContract.dragons(_dragonID);
+        require(currentAction == 0, "Dragon is busy!");
+        require(nextBlock2Action <= block.number, "Dragon is resting!");
         uint8 genAdd = 0xFF - uint8(bytes30(gensDragon)[_genNum]);
         uint240 newGens = gensDragon + (uint240(genAdd) << (29 - _genNum) * 8);
         _setResault(_dragonID, gensDragon, newGens);
     }
     function mutateFightGenRandom(uint256 _dragonID, uint256 _genNum) external payable {
-        //add check to actions and refactor it
-        require(mutagenContract.balanceOf(msg.sender) >= mutagenCount);
-        require(msg.value >= priceMutagenWork);
-        require(mainContract.ownerOf(_dragonID) == msg.sender);
-        require(_genNum <= 29);
-        uint256 returnValue = msg.value - priceMutagenWork;
-        wallet.transfer(priceMutagenWork);
-        if (returnValue > 0) {
-            msg.sender.transfer(returnValue);
-        }
+        mutagenContract.burn(msg.sender, mutagenCount);
+        _firstCheck(_dragonID, _genNum, priceMutagenWork);
         bytes32 random_number = RNG(addressRNG).get32b(msg.sender, _dragonID);
+        uint8 currentAction;
         uint240 gensDragon;
         uint256 nextBlock2Action;
-        (,,,gensDragon,nextBlock2Action) = mainContract.dragons(_dragonID);
-        require(nextBlock2Action <= block.number);
+        (,,currentAction,gensDragon,nextBlock2Action) = mainContract.dragons(_dragonID);
+        require(currentAction == 0, "Dragon is busy!");
+        require(nextBlock2Action <= block.number, "Dragon is resting!");
         uint240 genAdd = uint240(uint8(random_number[31]));
         if ((uint240(uint8(bytes30(gensDragon)[_genNum])) + genAdd) > 0xFF) {
             gensDragon -= uint240(uint256(1 << (30 - _genNum) * 8));
         }
         uint240 newGens = gensDragon + (genAdd << (29 - _genNum) * 8);
         _setResault(_dragonID, gensDragon, newGens);
-        mutagenContract.burn(msg.sender, priceMutagenWork);
     }
     function returnPrices() external view returns (uint256, uint256, uint256){
         return (mutagenCount, priceMutagenWork, priceMax);
     }
+    function _firstCheck(uint256 _dragonID, uint256 _genNum, uint256 _price) private {
+        require(msg.value >= _price, "Not enough ether!");
+        require(mainContract.ownerOf(_dragonID) == msg.sender, "Only the owner can do this!");
+        require(_genNum <= 29, "Number is to big!");
+        uint256 returnValue = msg.value - _price;
+        wallet.transfer(_price);
+        if (returnValue > 0) {
+            msg.sender.transfer(returnValue);
+        }
+    }
     function _setResault(uint256 _dragonID, uint240 _oldGens, uint240 _newGens) private {
         mainContract.changeDragonGen(_dragonID, _newGens, 1);
-        //mainContract.setTime2Rest(_dragonID, addTime2Rest);
+        if (addTime2Rest > 0) {
+            mainContract.setTime2Rest(_dragonID, addTime2Rest);
+        }        
         dragonsStatsContract.incGenLabFight(_dragonID);
         dragonsStatsContract.setLastAction(_dragonID, _dragonID, 12);
         emit FightGensChanged(msg.sender, _dragonID, _oldGens, _newGens);
