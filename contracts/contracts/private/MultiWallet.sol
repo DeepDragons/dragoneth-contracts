30ea688e41588fb284e61d4b2d90db08d9fd3430
Igor Khan <khanigot@gmail.com>
2020-02-21 20:21:43 +0500

    add ReentrancyGuard for flatting

--- contracts/contracts/private/MultiWallet.sol
+++ contracts/contracts/private/MultiWallet.sol
@@ -1,7 +1,35 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.6.1;
 
-import "../security/ReentrancyGuard.sol";
 
+/**
+ * @title Helps contracts guard agains reentrancy attacks.
+ * @author Remco Bloemen <remco@2π.com>
+ * @notice If you mark a function `nonReentrant`, you should also
+ * mark it `external`.
+ */
+contract ReentrancyGuard {
+
+  /**
+   * @dev We use a single lock for the whole contract.
+   */
+  bool private reentrancyLock = false;
+
+  /**
+   * @dev Prevents a contract from calling itself, directly or indirectly.
+   * @notice If you mark a function `nonReentrant`, you should also
+   * mark it `external`. Calling one nonReentrant function from
+   * another is not supported. Instead, you can implement a
+   * `private` function doing the actual work, and a `external`
+   * wrapper marked as `nonReentrant`.
+   */
+  modifier nonReentrant() {
+    require(!reentrancyLock);
+    reentrancyLock = true;
+    _;
+    reentrancyLock = false;
+  }
+
+}
 
 contract MultiWallet is ReentrancyGuard {
     address wallet1_7 = 0x98F04b28946C73D049c04A63Ac1F3352689F23bf; // Renat
